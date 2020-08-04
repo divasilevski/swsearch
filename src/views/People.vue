ff9ee5a169ca8a6f3d3beccccaab75c8a43d7eef
Di <divasilevski@gmail.com>
2020-08-04 15:41:16 +0300

    handle errors at people page

--- src/views/People.vue
+++ src/views/People.vue
@@ -1,12 +1,36 @@
 <template>
-  <div>
-    People page
+  <div v-if="!statusMessage">
+    <pre>{{ peopleFetch }}</pre>
+
   </div>
+
+  <div v-else>{{statusMessage}}</div>
 </template>
 
 <script>
 
 export default {
-  name: 'People'
+  name: 'People',
+  data: () => ({
+    peopleFetch: undefined,
+    statusMessage: "Загрузка..."
+  }),
+
+  created() {
+    const URL = `https://swapi.dev/api/people/${this.$route.params.id}`
+
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => {
+        if (data.detail) throw new Error(data.detail)
+      
+        this.peopleFetch = data
+        this.statusMessage = ''
+      })
+      .catch(error => {
+        this.statusMessage = `Что-то пошло не так. Ошибка ${error.message}`
+      })
+  }
+
 }
 </script>
