0015e52e2880f0f74bac4c2f1ba778494d8623da
Di <divasilevski@gmail.com>
2020-08-04 14:04:21 +0300

    fetch by search

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,12 +1,42 @@
 <template>
   <div>
-    Search page
+    {{searchResults}}
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'Search'
+  name: 'Search',
+  data: () => ({
+    searchResults: undefined,
+  }),
+
+  created() {
+    this.fetchData()
+  },
+
+  beforeRouteUpdate(to, from, next) {
+    next()
+    this.fetchData()
+  },
+
+  methods: {
+    fetchData() {
+      const queryParams = this.getQuery(this.$route.fullPath)
+      const URL = `https://swapi.dev/api/people${queryParams}`
+      
+      fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+          this.searchResults = data
+        })
+    },
+
+    getQuery(url) {
+      const splitedURL = url.split('?')
+      return `?${splitedURL[splitedURL.length - 1]}`
+    }
+  }
 }
 </script>
